f44e978502a21ade58d53f4e99bff5d0e574b8cc
Dillon Duncan <dillonduncan0607@gmail.com>
2025-04-07 13:59:38 -0500

    tabla para mostrar las tareas

--- practicas/src/components/ListaTareas.jsx
+++ practicas/src/components/ListaTareas.jsx
@@ -1,19 +1,31 @@
 import React from 'react'
 
-export default function ListaTareas({task}) {
+export default function ListaTareas({ task }) {
   return (
-    <div><ul>
-    {task.map((item) =>
-      <li key={item.id}>
-        {item.id}
-        <br />
-        {item.name}
-        <br />
-        {item.description}
-        <br />
-        {item.state !=false ? "Realizada" : "Pendiente"}
-      </li>
-    )}
-  </ul></div>
+    <div>   
+      <table class="table">
+        <caption>Lista de tareas</caption>
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Descripcion</th>
+            <th scope="col">Estado</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            task.map((item) =>
+              <tr>
+                <th key={item.id}>{item.id}</th>
+                <td>{item.name}</td>
+                <td>{item.description}</td>
+                <td>{item.state != false ? "Realizada" : "Pendiente"}</td>
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
+    </div>
   )
 }
